8dca0b470e2c3de9cc53d8747063464a51f817cb
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-11-09 18:12:34 +0100

    Django templates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
 
 # Create your views here.
+from .models import Question
+
 
 class IndexView(TemplateView):
     template_name = 'polls/index.html'
+
+    def questions(self):
+        return Question.objects.all()
