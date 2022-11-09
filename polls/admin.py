b8ecfc3872f4bc75a69b01cd03842739b28bd0e7
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-11-09 10:15:12 +0100

    Models & Admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
